b44be115d82d5f8b17a6c0dd0926b0baa3f89e73
Paul Downey <psd@whatfettle.com>
2025-10-21 10:56:26 +0100

    Working towards templates

--- bin/render.py
+++ bin/render.py
@@ -1,6 +1,23 @@
 import os
 import sys
+import csv
 from jinja2 import Environment, FileSystemLoader
+
+
+tables = {}
+
+
+def load(path, key, opt=None):
+    d = {}
+    for row in csv.DictReader(open(path, newline="")):
+        if (not opt) or opt(row):
+            d[row[key]] = row
+    return d
+
+
+def load_table(name, path, key=None, opt=None):
+    key = key or name
+    tables[name] = load(path, key, opt)
 
 
 def render(path, template, docs="docs", **kwargs):
@@ -16,5 +33,11 @@ def render(path, template, docs="docs", **kwargs):
 
 if __name__ == "__main__":
     environment = Environment(loader=FileSystemLoader("templates/"))
+
     template = environment.get_template("index.html")
     render("index.html", template)
+
+    load_table("organisation", "var/cache/organisation.csv")
+
+    for specification in ["intervention", "fund", "award"]:
+        load_table(specification, f"specification/{specification}.csv")
